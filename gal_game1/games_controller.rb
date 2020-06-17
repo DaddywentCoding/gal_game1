360ada772f785925ede8fef9402bf22dc913edbc
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-06-17 21:48:59 +0900

    combined girl.rb with boy.rb

--- gal_game1/games_controller.rb
+++ gal_game1/games_controller.rb
@@ -4,67 +4,65 @@ class GamesController
 
 	include GamesMessage
 
-	def game_play(**params)
-		build_characters(params)
+	def game_play(character)
+		build_characters(character)
 
 		day = 1
 
-		introduce_message(boy: @boy, girl: @girl)
+		introduce_message(character)
 
 		status_enter
 
-		status_message(boy: @boy, girl: @girl)
+		status_message(character)
 
 		story_enter
 
 		loop do
 			puts "#{day}日目"
-			@boy.decision_action_type(girl: @girl)
+			@character.decision_action_type(character)
 			
 			status_enter
 
 			value_flat
 
-			status_message(boy: @boy, girl: @girl)
+			status_message(character)
 			break if game_play_end?
 
 			day += 1
 			story_enter
 		end
-		game_play_jadgment
+		game_play_jadgment(character)
 	end
 
-	def build_characters(**params)
-		@boy = params[:boy]
+	def build_characters(character)
+		@character = character
 
-		input_boy_name(boy: @boy)
+		input_character_name1(character)
 
-		@girl = params[:girl]
-		
-		input_girl_name(girl: @girl)
+		input_character_name2(character)
 	end
 
 	def value_flat
-		@boy.hp = 0 if @boy.hp < 0
-		@girl.love_grade = 10 if @girl.love_grade > 10
+		@character.hp = 0 if @character.hp < 0
+		@character.love_grade = 10 if @character.love_grade > 10
 	end
 
 	def game_play_end?
-		@boy.hp <= 0 || @girl.love_grade >= 10
+		@character.hp <= 0 || @character.love_grade >= 10
 	end
 
 	def girl_likes_him?
-		@girl.love_grade == 10
+		@character.love_grade == 10
 	end
 
-	def game_play_jadgment
+	def game_play_jadgment(character)
 		if girl_likes_him?
 
-			jadgment_true_message(boy: @boy, girl: @girl)
+			jadgment_true_message(character)
 			
 		else
 
-			jadgment_bad_message(boy: @boy, girl: @girl)
+			jadgment_bad_message(character)
 			
 		end
 	end
