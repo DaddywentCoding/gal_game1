761042927c227c03469383846dab7efc7b6d4200
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-06-21 20:58:57 +0900

    fixed_overall

--- gal_game1/character_message.rb
+++ gal_game1/character_message.rb
@@ -1,227 +1,331 @@
 module CharacterMessage
 
-	def action_message(character, action_num)
+	def decision_message_and_flu(boy, girl, action_num)
 
 		case action_num
 		when 1
 			puts <<~EOS
-			------------------------------------
-			#{character.name1}には#{character.name2}を想い、
+			-----------------------------------
+			#{boy.name}には#{girl.name}を想い、
 			眠れない夜がある。恋とは苦しいものだ。
-			#{character.name1}は自分に心酔していた。
+			#{boy.name}は自分に心酔していた。
 			------------------------------------
 			EOS
 
+			hp_flu = -1
+			love_flu = +1
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 2
 			puts <<~EOS
 			-------------------------------------
-			#{character.name1}はよく一人でギャグをかます。
-			#{character.name2}が笑ってくれるからだ。
+			#{boy.name}はよく一人でギャグをかます。
+			#{girl.name}が笑ってくれるからだ。
 			そう。僕は、愛の道化師。
 			-------------------------------------
 			EOS
 
-		when 3
-			puts <<~EOS
-			-------------------------------------
-			#{character.name1}は#{character.name2}のスカートをめくった。
-			#{character.name1}はビンタを食らった。#{character.name2}は
-			おしとやかに見えて般若みたいだなぁ。だが、いい。
-			#{character.name1}はMだった。
-			-------------------------------------
-			EOS
+			hp_flu = -1
+			love_flu = +2
+
+			calculate(boy, girl, hp_flu, love_flu)
 
 		when 3
 			puts <<~EOS
 			-------------------------------------
-			#{character.name1}は#{character.name2}とオセロ対決をした。
-			#{character.name1}は10連敗した。けど、楽しそうな
-			#{character.name2}を見れたので嬉しかった。
+			#{boy.name}は#{girl.name}のスカートをめくった。
+			#{boy.name}はビンタを食らった。#{girl.name}は
+			おしとやかに見えて般若みたいだなぁ。だが、いい。
+			#{boy.name}はMだった。
 			-------------------------------------
 			EOS
+
+			hp_flu = -1
+			love_flu = +3
+
+			calculate(boy, girl, hp_flu, love_flu)
+
+		when 3
+			puts <<~EOS
+			-------------------------------------
+			#{boy.name}は#{girl.name}とオセロ対決をした。
+			#{boy.name}は10連敗した。けど、楽しそうな
+			#{girl.name}を見れたので嬉しかった。
+			-------------------------------------
+			EOS
+
+			hp_flu = -2
+			love_flu = +1
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 4
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}に誕生日プレゼント
+			#{boy.name}は#{girl.name}に誕生日プレゼント
 			をあげた。誕生日は昨日だったらしいけど、喜んで
 			くれたからいっか。
 			-------------------------------------
 			EOS
+
+			hp_flu = -2
+			love_flu = +2
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 5
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は立ち姿で#{character.name2}を魅了した。
+			#{boy.name}は立ち姿で#{girl.name}を魅了した。
 			見た目はは小学6年生。精神は42歳。
-			#{character.name2}はおじ好きだった。
+			#{girl.name}はおじ好きだった。
 			-------------------------------------
 			EOS
+
+			hp_flu = -2
+			love_flu = +3
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 6
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}が好きすぎて
-			心臓発作になった。けど、#{character.name2}
+			#{boy.name}は#{girl.name}が好きすぎて
+			心臓発作になった。けど、#{girl.name}
 			が看病してくれたので生き返った。
 			-------------------------------------
 			EOS
+
+			hp_flu = -3
+			love_flu = +1
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 7
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は筋トレにハマった。
-			動機は不純だった。全ては#{character.name2}に
-			モテるためだった。その潔さも#{character.name1}の良さ。
+			#{boy.name}は筋トレにハマった。
+			動機は不純だった。全ては#{girl.name}に
+			モテるためだった。その潔さも#{boy.name}の良さ。
 			-------------------------------------
 			EOS
+
+			hp_flu = -3
+			love_flu = +2
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 8
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は世界平和を願った。人はなぜ争い、
-			憎み合うのか？僕は#{character.name2}さえいれば
+			#{boy.name}は世界平和を願った。人はなぜ争い、
+			憎み合うのか？僕は#{girl.name}さえいれば
 			心の安寧が保たれる。。。そう思った。
 			-------------------------------------
 			EOS
+
+			hp_flu = -3
+			love_flu = +3
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 9
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は天下一武道会に参戦した。満月を見て
-			大猿に変身するのかと思いきや、#{character.name2}
+			#{boy.name}は天下一武道会に参戦した。満月を見て
+			大猿に変身するのかと思いきや、#{girl.name}
 			みたいに綺麗だなと思った。
 			-------------------------------------
 			EOS
+
+			hp_flu = 0
+			love_flu = +1
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 10
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}に壁ドンした。
-			#{character.name2}は真顔だった。壁ドンはあまり
+			#{boy.name}は#{girl.name}に壁ドンした。
+			#{girl.name}は真顔だった。壁ドンはあまり
 			好きではないのか？自分がが単にタイプではない
 			のか悩んだ。
 			-------------------------------------
 			EOS
+
+			hp_flu = 0
+			love_flu = +2
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 11
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}にフラッシュモブ
-			を披露した。#{character.name2}が嬉し涙を流した。
+			#{boy.name}は#{girl.name}にフラッシュモブ
+			を披露した。#{girl.name}が嬉し涙を流した。
 			趣向が掴めない。
 			-------------------------------------
 			EOS
+
+			hp_flu = -0
+			love_flu = +3
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 12
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}に「おはよう」と
-			挨拶をした。対して#{character.name2}の挨拶は
+			#{boy.name}は#{girl.name}に「おはよう」と
+			挨拶をした。対して#{girl.name}の挨拶は
 			「チワッ」だった。体育会系の挨拶は良い。
 			-------------------------------------
 			EOS
+
+			hp_flu = 1
+			love_flu = 0
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 13
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は親とショッピングモールに行った。
-			そこで#{character.name2}と偶然出会った。
+			#{boy.name}は親とショッピングモールに行った。
+			そこで#{girl.name}と偶然出会った。
 			運命だと思った。
 			-------------------------------------
 			EOS
+
+			hp_flu = 2
+			love_flu = 0
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 14
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}はある噂を耳にした。#{character.name2}
+			#{boy.name}はある噂を耳にした。#{girl.name}
 			が自分のことを好きらしい。シンプルに嬉しかった。
 			-------------------------------------
 			EOS
+
+			hp_flu = 3
+			love_flu = 0
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 15
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}はミュージシャンになった。職業柄モテ
-			まくる。目移りしそうになったが、結局、#{character.name2}
-			一筋だった。#{character.name1}のいいとこでた。
+			#{boy.name}はミュージシャンになった。職業柄モテ
+			まくる。目移りしそうになったが、結局、#{boy.name}
+			一筋だった。#{girl.name}のいいとこでた。
 			-------------------------------------
 			EOS
+
+			hp_flu = 1
+			love_flu = +1
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 16
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は河川敷で#{character.name2}への愛を
+			#{boy.name}は河川敷で#{girl.name}への愛を
 			叫んだ。そして、偶然向こう岸を歩いている
-			#{character.name2}に届いた。続きはCMの後。
+			#{girl.name}に届いた。続きはCMの後。
 			-------------------------------------
 			EOS
+
+			hp_flu = 1
+			love_flu = +2
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 17
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}とデートした。
-			#{character.name2}は、ひどくマブイ女だった。
-			#{character.name1}は#{character.name2}の笑顔を守り
+			#{boy.name}は#{girl.name}とデートした。
+			#{girl.name}は、ひどくマブイ女だった。
+			#{boy.name}は#{girl.name}の笑顔を守り
 			続けると心の奥底で誓ったのであった。
 			-------------------------------------
 			EOS
+
+			hp_flu = 1
+			love_flu = +3
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 18
 			puts <<~EOS
 			------------------------------------
-			困ってる女の人を#{character.name1}が助けた。
-			その女の人は#{character.name2}のお母さんだった
+			困ってる女の人を#{boy.name}が助けた。
+			その女の人は#{girl.name}のお母さんだった
 			計画通り。
 			-------------------------------------
 			EOS
+
+			hp_flu = -1
+			love_flu = +1
+
+			calculate(boy, girl, hp_flu, love_flu)
 		when 19
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}は#{character.name2}を家に連れ込んだ。
+			#{boy.name}は#{girl.name}を家に連れ込んだ。
 			これから起こることを考えると下半身が熱くなった。
 			僕は大人の階段を登るんだ。
 			-------------------------------------
 			EOS
+
+			hp_flu = -1
+			love_flu = +2
+
+			calculate(boy, girl, hp_flu, love_flu)
 		end
 	end
 
-	def flu_message(hp_flu, love_flu, character)
+	def value_flat(boy, girl)
+		boy.hp = 0 if boy.hp < 0
+		girl.love_grade = 10 if girl.love_grade > 10
+	end
+
+	def flu_message(boy, girl, hp_flu, love_flu)
 		if hp_flu > 0 && love_flu > 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}のHPが#{hp_flu}あがった。
-			#{character.name2}の好感度が#{love_flu}あがった。
+			#{boy.name}のHPが#{hp_flu}あがった。
+			#{girl.name}の好感度が#{love_flu}あがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu < 0 && love_flu > 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}のHPが#{hp_flu}さがった。
-			#{character.name2}の好感度が#{love_flu}あがった。
+			#{boy.name}のHPが#{hp_flu}さがった。
+			#{girl.name}の好感度が#{love_flu}あがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu > 0 && love_flu < 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}のHPが#{hp_flu}あがった。
-			#{character.name2}の好感度が#{love_flu}さがった。
+			#{boy.name}のHPが#{hp_flu}あがった。
+			#{girl.name}の好感度が#{love_flu}さがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu < 0 && love_flu < 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}のHPが#{hp_flu}さがった。
-			#{character.name2}の好感度が#{love_flu}さがった。
+			#{boy.name}のHPが#{hp_flu}さがった。
+			#{girl.name}の好感度が#{love_flu}さがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu == 0 && love_flu > 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name2}の好感度が#{love_flu}あがった。
+			#{girl.name}の好感度が#{love_flu}あがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu == 0 && love_flu < 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name2}の好感度が#{love_flu}さがった。
+			#{girl.name}の好感度が#{love_flu}さがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu > 0 && love_flu == 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}のHPが#{hp_flu}あがった。
+			#{boy.name}のHPが#{hp_flu}あがった。
 			-------------------------------------
 			EOS
 		elsif hp_flu < 0 && love_flu == 0
 			puts <<~EOS
 			------------------------------------
-			#{character.name1}のHPが#{hp_flu}さがった。
+			#{boy.name}のHPが#{hp_flu}さがった。
 			-------------------------------------
 			EOS
 		end	
--- gal_game1/games_controller.rb
+++ gal_game1/games_controller.rb
@@ -1,68 +1,78 @@
-require "./message_games_controller"
+require "./games_message"
 
 class GamesController
 
 	include GamesMessage
 
-	def game_play(character)
-		build_characters(character)
+	def initialize
+    @boy = Character.new(name: "", hp: 10, love_grade: 0)
+		@girl = Character.new(name: "", hp: 10, love_grade: 0)
+    @day = 1
 
-		day = 1
+    input_boy_name
+		input_girl_name
+   end
 
-		introduce_message(character)
+	def game_play
+
+		introduce_message(@boy, @girl)
 
 		status_enter
 
-		status_message(character)
+		status_message(@boy, @girl)
 
 		story_enter
 
 		loop do
-			puts "#{day}日目"
-			@character.decision_action_type(character)
+			puts "#{@day}日目"
+			@boy.boy_action(@girl)
 			
 			status_enter
 
-			value_flat
-
-			status_message(character)
+			status_message(@boy, @girl)
 			break if game_play_end?
 
-			day += 1
+			@day += 1
 			story_enter
 		end
-		game_play_jadgment(character)
+		game_play_jadgment
 	end
 
-	def build_characters(character)
-		@character = character
 
-		input_character_name1(character)
+	private
 
-		input_character_name2(character)
+	def input_boy_name
+		loop do
+      puts "主人公の名前を入力してください"
+      @boy.name = gets.chomp
+      break @boy.name unless @boy.name.empty?
+     end
 	end
 
-	def value_flat
-		@character.hp = 0 if @character.hp < 0
-		@character.love_grade = 10 if @character.love_grade > 10
+	def input_girl_name
+		loop do
+      puts "ヒロインの名前を入力してください"
+      @girl.name = gets.chomp
+      break @girl.name unless @girl.name.empty?
+     end
 	end
 
 	def game_play_end?
-		@character.hp <= 0 || @character.love_grade >= 10
+		@boy.hp <= 0 || @girl.love_grade >= 10
 	end
 
 	def girl_likes_him?
-		@character.love_grade == 10
+		@girl.love_grade == 10
 	end
 
-	def game_play_jadgment(character)
+	def game_play_jadgment
 		if girl_likes_him?
 
-			jadgment_true_message(character)
+			jadgment_true_message(@boy, @girl)
 			
 		else
 
-			jadgment_bad_message(character)
+			jadgment_bad_message(@boy, @girl)
 			
 		end
 	end
